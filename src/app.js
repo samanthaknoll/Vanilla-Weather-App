e42f548ab3b6ded71ffd7dc21910894f88931cd4
Samantha Knoll <samanthaknoll20@gmail.com>
2023-03-05 04:49:59 -0500

    Update JS with lat & lon API

--- src/app.js
+++ src/app.js
@@ -41,7 +41,8 @@ let dayTime = document.querySelector("h2");
 dayTime.innerHTML = `Last Updated: ${day}, ${month} ${date} at ${hours}:${minutes}`;
 
 /// Daily Forecast
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -84,12 +85,23 @@ function displayWeatherCondition(response) {
   showIcon(response);
 }
 
+///Weekly Forecast API
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "97f8e93f00107773f88eafd933ce86b7";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function showIcon(response) {
   let iconElement = document.querySelector("#icon");
   let iconCode = response.data.weather[0].icon;
   let iconUrl = `http://openweathermap.org/img/wn/${iconCode}.png`;
   iconElement.setAttribute("src", iconUrl);
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function searchCity(city) {
